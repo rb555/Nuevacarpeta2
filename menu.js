f07fe748602542c29d762e239d5fe02451890f4e
Rocío Baigorria <97059658+rb555@users.noreply.github.com>
2022-11-17 22:19:45 -0300

    version actualizada de portfolio

---
+++ menu.js
@@ -0,0 +1,8 @@
+var menulateral = document.getElementById("menulateral");
+
+function abrirMenu(){
+    menulateral.style.right = "0";
+}
+function cerrarMenu(){
+    menulateral.style.right = "-200px";
+}
